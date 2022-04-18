4e8daee3e54cdad49539d55ba146b8c0018e3bb3
Victor <xrlabs.mephi@gmail.com>
2022-04-18 22:00:43 +0300

    Postgresql settings

--- Python_Django_CookBook/settings.py
+++ Python_Django_CookBook/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Python_Django_CookBook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'django',
+    'USER': 'xrlabs',
+    'PASSWORD': '12345678',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
     }
 }
 
