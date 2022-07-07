3a0fbe68051a51bceda62353b2e96baf763a19c5
jBuly4 <ibuly4@gmail.com>
2022-07-07 19:21:35 +0300

    added navbar, templates and views for listing tags, ingreds, corrected urls

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -5,7 +5,10 @@ app_name = 'cookbook'
 
 urlpatterns = [
     path('', views.recipe_lst, name='recipe_lst'),
+    path('ingredients/', views.ingredient_lst, name='ingredients'),  # dunno why, but if it is after recipe_detail
+    # link then views.recipe_detail raises error
+    path('tags/', views.tag_lst, name='tags'),
     path('<str:title>/', views.recipe_detail, name='recipe_detail'),  # Django3 by example p29
-    path('ingredient/<str:title>/', views.ingredient_detail, name='ingredient_detail'),
+    path('ingredients/<str:title>/', views.ingredient_detail, name='ingredient_detail'),
     path('tags/<str:title>/', views.tags_view, name='tags_page'),
 ]
